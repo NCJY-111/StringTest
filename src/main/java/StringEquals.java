6113fa79b43ab1aebc6d40729fd22d05ce5b9b01
NCJY-111 <nicjos-1@student.ltu.se>
2022-04-01 11:54:08 +0200

    Initial commit Two String test

---
+++ src/main/java/StringEquals.java
@@ -0,0 +1,7 @@
+public class StringEquals {
+
+        String Char = "ABC";
+        String Char2 = "ABCD";
+
+
+}
---
+++ src/test/java/StringEqualsTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringEqualsTest {
+
+
+@Test
+    void charTest() {
+    StringEquals stringEquals = new StringEquals();
+
+
+    assertSame("ABC",stringEquals.Char);
+}
+
+}
